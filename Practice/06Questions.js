764274433c81fe32c2472f305e15460acad0a4c3
Asad-bot07 <asadhussain2408@gmail.com>
2025-08-31 01:54:02 +0530

    06 ques code added

---
+++ Practice/06Questions.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Write a function that accepts an array of numbers and returns the first element.
+function isFirst(arr) {
+    return arr[0];
+}
+let arr = [1, 2, 3, 4, 5];
+console.log(isFirst(arr));
---
+++ Practice/06Questions.ts
@@ -0,0 +1,6 @@
+// Write a function that accepts an array of numbers and returns the first element.
+function isFirst(arr:number[]) : number | undefined{
+    return arr[0]
+}
+let arr:number[] = [1,2,3,4,5]
+console.log(isFirst(arr));
