30b834bf924cb8facde437a4807d7a00e09f9b8e
Asad-bot07 <asadhussain2408@gmail.com>
2025-08-31 02:02:16 +0530

    Ques 7 code added

---
+++ Practice/07Questions.js
@@ -0,0 +1,5 @@
+"use strict";
+// Create a tuple that stores a string and a number (example: "Asad", 19). Access both elements.
+Object.defineProperty(exports, "__esModule", { value: true });
+let tup = [19, "Asad"];
+console.log(`Number : ${tup[0]} , String : ${tup[1]}`);
---
+++ Practice/07Questions.ts
@@ -0,0 +1,4 @@
+// Create a tuple that stores a string and a number (example: "Asad", 19). Access both elements.
+
+let tup : [number,string] = [19,"Asad"]
+console.log(`Number : ${tup[0]} , String : ${tup[1]}`);
