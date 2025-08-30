f2d990aad1ac4c60e7e4de4dec0086c1d40357af
Asad-bot07 <asadhussain2408@gmail.com>
2025-08-31 01:41:01 +0530

    Solved problems added

---
+++ Practice/05Questions.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Declare an array of numbers and an array of strings.
+let arr = [1, 2, 3, 4, 5];
+let arr1 = ["Asad", "TypeScript", "JavaScript"];
+console.log(`Numbers : ${arr}`);
+console.log(`Strings : ${arr1}`);
---
+++ Practice/05Questions.ts
@@ -0,0 +1,5 @@
+// Declare an array of numbers and an array of strings.
+let arr:number[] = [1,2,3,4,5]
+let arr1:string[] = ["Asad","TypeScript","JavaScript"]
+console.log(`Numbers : ${arr}`);
+console.log(`Strings : ${arr1}`);
