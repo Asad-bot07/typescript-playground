3be45e8228ea1a72e6bd0e897c50b44bb0e1043b
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-03 02:02:27 +0530

    Started with OOPs

---
+++ Core/05GettingStartedWithOOPs.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// a class is created using the keyword "class" followed by <Classname>
+class Device {
+    constructor() {
+        this.name = "Samsung";
+        this.price = 43000;
+        this.category = "touchscreen";
+    }
+}
+let a55 = new Device();
+let s24 = new Device();
---
+++ Core/05GettingStartedWithOOPs.ts
@@ -0,0 +1,8 @@
+// a class is created using the keyword "class" followed by <Classname>
+class Device{
+    name = "Samsung";
+    price = 43000;
+    category = "touchscreen";
+}
+let a55 = new Device()
+let s24 = new Device()
