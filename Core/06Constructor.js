5d7a8cfdd3d9a84c28b8806f45d899d9ca2e54f3
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-03 02:16:52 +0530

    Constructor with TS added

--- Core/06Constructor.js
+++ Core/06Constructor.js
@@ -1,2 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// the keyword public is an access specifier which make sure that the var can be accesed by the user outside the class
+class Factory {
+    constructor(product, price) {
+        this.product = product;
+        this.price = price;
+        console.log(product);
+        console.log(price);
+    }
+}
+// Constructor is nothing but a function which gets automatically called when created a object
+const Samsung = new Factory("A55", 43000);
---
+++ Core/06Constructor.ts
@@ -0,0 +1,11 @@
+// the keyword public is an access specifier which make sure that the var can be accesed by the user outside the class
+class Factory{
+    constructor(public product : string, public price : number){
+        console.log(product);
+        console.log(price);
+    }
+}
+
+// Constructor is nothing but a function which gets automatically called when created a object
+
+const Samsung = new Factory("A55",43000);
