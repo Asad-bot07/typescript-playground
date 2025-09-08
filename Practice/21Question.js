9aa39a83690d9afa2a4a818bd16365759ce32f1f
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-08 21:23:12 +0530

    21 Question code added

---
+++ Practice/21Question.js
@@ -0,0 +1,7 @@
+"use strict";
+// 21. Write a function that returns void
+Object.defineProperty(exports, "__esModule", { value: true });
+const add = (a, b) => {
+    console.log(a + b);
+};
+add(5, 6);
---
+++ Practice/21Question.ts
@@ -0,0 +1,7 @@
+// 21. Write a function that returns void
+
+const add = (a : number, b : number) : void => {
+    console.log(a+b);
+}
+
+add(5, 6)
