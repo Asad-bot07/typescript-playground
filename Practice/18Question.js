42a14d9a41eb3acfa4d76e6faf5b0a09c53c120a
Asad-bot07 <asadhussain2408@gmail.com>
2025-09-08 21:12:02 +0530

    Question 18 code added

---
+++ Practice/18Question.js
@@ -0,0 +1,7 @@
+"use strict";
+// 18. Write a function that adds two numbers and returns the result.
+Object.defineProperty(exports, "__esModule", { value: true });
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(5, 6));
---
+++ Practice/18Question.ts
@@ -0,0 +1,8 @@
+// 18. Write a function that adds two numbers and returns the result.
+
+const add = (a: number, b : number) : number => {
+    return a+b
+}
+
+console.log(add(5, 6));
+
